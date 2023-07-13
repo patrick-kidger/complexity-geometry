02432860740f78b321bbebe7bd7aa64db40fb0ed
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-07-13 12:11:07 -0700

    tweaks

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
-import equinox as eqx
 import diffrax as dfx
+import equinox as eqx
 import jax
-import jax.flatten_util as jfu
-import jax.lax as lax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
+import optax
 import optimistix as optx
 from collections.abc import Callable
-from equinox.internal import ω
-from jaxtyping import Array, Float
-from mpl_toolkits.mplot3d import Axes3D
+from jaxtyping import Array, Float, Int
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 from typing import TypeAlias
 
 
@@ -18,37 +16,32 @@ jax.config.update("jax_enable_x64", True)
 
 
 # M = 4^N - 1 for a system of N qubits.
-Scalar: TypeAlias = Float[Array, ""]
-ArrayM: TypeAlias = Float[Array, "M"]
+FloatScalar: TypeAlias = Float[Array, ""]
+Array1D: TypeAlias = Float[Array, " _"]
+ArrayM: TypeAlias = Float[Array, " M"]
+ArrayMM: TypeAlias = Float[Array, "M M"]
 ArrayMMM: TypeAlias = Float[Array, "M M M"]
 
 
 def _vector_field(
-    t: Scalar, y: tuple[ArrayM, ArrayM], Γ: Callable[[ArrayM], ArrayMMM]
+    t: FloatScalar, y: tuple[ArrayM, ArrayM], christoffel: Callable[[ArrayM], ArrayMMM]
 ) -> tuple[ArrayM, ArrayM]:
     x, v = y
     dx = v
-    dv = -jnp.einsum("IJK,J,K->I", Γ(x), v, v)
+    dv = -jnp.einsum("IJK,J,K->I", christoffel(x), v, v)
     return dx, dv
 
 
 def _solve_ivp(
-    t0: Scalar,
-    t1: Scalar,
+    t0: FloatScalar,
+    t1: FloatScalar,
     x0: ArrayM,
     v0: ArrayM,
-    Γ: Callable[[ArrayM], ArrayMMM],
+    christoffel: Callable[[ArrayM], ArrayMMM],
     saveat: dfx.SaveAt,
 ) -> dfx.Solution:
-
-    # Handle singularities: convert inf to max, and -inf to min.
-    # Then 0 * inf produces 0, not NaN.
-    def Γ_clipped(x):
-        info = jnp.finfo(jnp.result_type(x))
-        return jnp.clip(Γ(x), a_min=info.min, a_max=info.max)
-
     term = dfx.ODETerm(_vector_field)
-    solver = dfx.Tsit5()
+    solver = dfx.Tsit5(scan_kind="lax")  # TODO
     dt0 = None
     y0 = (x0, v0)
     stepsize_controller = dfx.PIDController(
@@ -61,21 +54,24 @@ def _solve_ivp(
         t1,
         dt0,
         y0,
-        args=Γ_clipped,
+        args=christoffel,
         stepsize_controller=stepsize_controller,
         saveat=saveat,
-        throw=False,
+        adjoint=dfx.DirectAdjoint(),  # TODO
+        max_steps=16**5,
     )
-    # TODO: raise error with JAX
-    lax.cond(eqx.internal.unvmap_all(sol.result == dfx.RESULTS.successful), lambda: None, lambda: jax.debug.breakpoint())
     return sol
 
 
-def solve_ivp_t1(
-    t0: Scalar, t1: Scalar, x0: ArrayM, v0: ArrayM, Γ: Callable[[ArrayM], ArrayMMM]
+def _solve_ivp_t1(
+    t0: FloatScalar,
+    t1: FloatScalar,
+    x0: ArrayM,
+    v0: ArrayM,
+    christoffel: Callable[[ArrayM], ArrayMMM],
 ) -> tuple[ArrayM, ArrayM]:
     saveat = dfx.SaveAt(t1=True)
-    sol = _solve_ivp(t0, t1, x0, v0, Γ, saveat)
+    sol = _solve_ivp(t0, t1, x0, v0, christoffel, saveat)
     (xs, vs) = sol.ys
     (x1,) = xs
     (v1,) = vs
@@ -84,132 +80,293 @@ def solve_ivp_t1(
 
 @eqx.filter_jit
 def solve_ivp_dense(
-    t0: Scalar, t1: Scalar, x0: ArrayM, v0: ArrayM, Γ: Callable[[ArrayM], ArrayMMM]
-) -> Callable[[Scalar], tuple[ArrayM, ArrayM]]:
+    t0: FloatScalar,
+    t1: FloatScalar,
+    x0: ArrayM,
+    v0: ArrayM,
+    christoffel: Callable[[ArrayM], ArrayMMM],
+) -> Callable[[FloatScalar], ArrayM]:
     saveat = dfx.SaveAt(dense=True)
-    sol = _solve_ivp(t0, t1, x0, v0, Γ, saveat)
+    sol = _solve_ivp(t0, t1, x0, v0, christoffel, saveat)
     # Drop the velocity component
     return eqx.Partial(lambda f, x: f(x)[0], sol.evaluate)
 
 
-class _ClippedGradient(optx.AbstractDescent):
-    def __call__(self, step_size, args, options):
-        vector = options["vector"]
-        diff = (vector**ω / optx.max_norm(vector)).ω
-        return (-step_size * diff**ω).ω, optx.RESULTS.successful
+def _solve_init(
+    init_init_v0: ArrayM,
+    init_x0: ArrayM,
+    tdiff: ArrayM,
+    christoffel: Callable[[ArrayM], ArrayMMM],
+) -> ArrayM:
+    def _init_root_fn(init_v0: ArrayM, _) -> ArrayM:
+        Γ = christoffel(init_x0)
+        return (
+            init_v0
+            + 0.5 * tdiff * jnp.einsum("IJK,J,K->I", Γ, init_v0, init_v0)
+            - init_init_v0
+        )
 
-
-class _ClippedGradientDescent(optx.AbstractGradientDescent):
-    def __init__(self, rtol: float, atol: float, learning_rate: float):
-        self.rtol = rtol
-        self.atol = atol
-        self.norm = optx.max_norm
-        self.line_search = optx.LearningRate(_ClippedGradient(), learning_rate)
+    root_finder = optx.Newton(rtol=1e-4, atol=1e-4)
+    init_sol = optx.least_squares(_init_root_fn, root_finder, init_init_v0)
+    return init_sol.value
 
 
 @eqx.filter_jit
 def solve_bvp(
     target: ArrayM,
     pieces: int,
-    Γ: Callable[[ArrayM], ArrayMMM],
+    christoffel: Callable[[ArrayM], ArrayMMM],
     canonicalise: None | Callable[[ArrayM], ArrayM] = None,
 ) -> ArrayM:
     ts = jnp.linspace(0, 1, pieces + 1)
     t0s = ts[:-1]
     t1s = ts[1:]
     (M,) = target.shape
-    init_later_x0s = jax.vmap(
-        lambda x: jnp.linspace(0, x, pieces + 1, dtype=target.dtype)[1:-1], out_axes=1
+    init_x0s: Float[Array, "pieces+1 M"] = jax.vmap(
+        lambda x: jnp.linspace(0, x, pieces + 1, dtype=target.dtype), out_axes=1
     )(target)
-    # TODO: initialise better
-    init_v0s = jnp.zeros((pieces, M), target.dtype)
+    init_later_x0s: Float[Array, "pieces-1 M"] = init_x0s[1:-1]
+    # For our multiple-shooting solve, we need to initialise both positions and
+    # velocity at each of our boundary points.
+    # We initialise positions as above: linearly interpolate in the space of intrinsic
+    # coordinates.
+    # Now to initialise velocities. We want these to approximately solve the shooting
+    # problem over each interval, so that we take as few iterative steps as possible.
+    #
+    # Considering a single shooting interval, make a one-step Euler approximation over
+    # the whole interval (here, `i` denote indexing over channels, and we elide the
+    # indexing over intervals):
+    #
+    # x1_i = x0_i + \int_t0^t1 v_i(t) dt
+    #      = x0_i + \int_t0^t1 v_i(t0) + \int_t0^t Γ^i_jk(s) v_j(s) v_k(s) ds dt
+    #      ~ x0_i + \int_t0^t1 v_i(t0) + \int_t0^t Γ^i_jk(t0) v_j(t0) v_k(t0) ds dt
+    #      = x0_i + \int_t0^t1 v_i(t0) + (t - t0) Γ^i_jk(t0) v_j(t0) v_k(t0) dt
+    #      = x0_i + (t1 - t0) v_i(t0) + 0.5 (t1 - t0)^2 Γ^i_jk(t0) v_j(t0) v_k(t0)
+    #                                                                                [A]
+    # We see that an estimate of v(t0) is given by solving this multivariate
+    # quadratric system. (Of the form 0 = A_ijk y_j y_k + B_ij y_j + c_i.)
+    #
+    # Now this is a difficult in general (unlike the linear case! i.e. A=0). See:
+    #   https://mathoverflow.net/questions/153436/can-you-efficiently-solve-a-system-of-quadratic-multivariate-polynomials
+    #   https://arxiv.org/abs/cs/0403008
+    #   https://hal.science/hal-01567408/document
+    #
+    # So, the answer is to solve this in turn as a root-finding problem.
+    #
+    # Now we note that our Γ is often sparse/small. So to solve this root-finding
+    # problem in turn, we start off our iteration with v(t0) = (x1 - x0)/(t1 - t0).
+    # (Recall that we are still considering just a single shooting interval.). Then
+    # run a root-finding algorithm wrt [A] over every interal.
+    #
+    # Then returning to our overall problem: use *that* to initialise our multiple
+    # shooting algorithm!
+    tdiffs: Float[Array, "pieces 1"] = (t1s - t0s)[:, jnp.newaxis]
+    init_init_v0s: Float[Array, "pieces M"] = (init_x0s[1:] - init_x0s[:-1]) / tdiffs
+    # For efficiency, solve this as a batch-of-root-finding-problems, not just a single
+    # big root-finding problem. (Scales as O(BM^3) rather than O((BM)^3).)
+    init_v0s = eqx.filter_vmap(_solve_init)(
+        init_init_v0s, init_x0s[:-1], tdiffs, christoffel
+    )
+
     if canonicalise is not None:
         target = canonicalise(target)
 
-    def _root_fn(later_x0s: Float[Array, "M-1"], v0s: ArrayM):  # 2M - 1 inputs
+    def _root_fn(later_x0s: Float[Array, "pieces-1 M"], v0s: Float[Array, "pieces M"]):  # M * (2 * pieces - 1) inputs
         zero = jnp.zeros((1, M), target.dtype)
         x0s = jnp.concatenate([zero, later_x0s])
-        (x1s, v1s) = eqx.filter_vmap(solve_ivp_t1)(t0s, t1s, x0s, v0s, Γ)
+        (x1s, v1s) = eqx.filter_vmap(_solve_ivp_t1)(t0s, t1s, x0s, v0s, christoffel)
         if canonicalise is not None:
             x0s = jax.vmap(canonicalise)(x0s)
             x1s = jax.vmap(canonicalise)(x1s)
-        x_diff = x0s[1:] - x1s[:-1]  # M - 1 conditions
-        v_diff = v0s[1:] - v1s[:-1]  # M - 1 conditions
-        target_diff = x1s[-1] - target  # 1 condition
-        return x_diff, v_diff, target_diff  # 2M - 1 outputs
+        x_diff = x0s[1:] - x1s[:-1]  # M * (pieces - 1) conditions
+        v_diff = v0s[1:] - v1s[:-1]  # M * (pieces - 1) conditions
+        target_diff = x1s[-1] - target  # M conditions
+        return x_diff, v_diff, target_diff  # M * (2 * pieces - 1) outputs
 
-    def _min_fn(z0, args):
-        later_x0s, v0s = z0
-        out, _ = jfu.ravel_pytree(_root_fn(later_x0s, v0s))
-        loss = jnp.mean(out**2)
-        return loss
+    def _lstsq_fn(z, _):
+        return _root_fn(*z)
 
-    # TODO: substitute for OptaxMinimiser?
-    solver = _ClippedGradientDescent(rtol=1e-8, atol=1e-8, learning_rate=1e-2)
-    sol = optx.minimise(_min_fn, solver, (init_later_x0s, init_v0s), max_steps=1000)
-    later_x0s, v0s = sol.value
-    return v0s
+    # TODO
+    # solver = optx.OptaxMinimiser(optax.adabelief, rtol=1e-4, atol=1e-4, learning_rate=1e-2)
+    solver = optx.Newton(rtol=1e-4, atol=1e-4)
+    sol = optx.least_squares(
+        _lstsq_fn, solver, (init_later_x0s, init_v0s), max_steps=10000, throw=False
+    )
+    _, v0s = sol.value
+    return v0s[0]
 
 
-def demo_sphere():
-    # θ_target = 2.1
-    # φ_target = 0.8
-    θ_target = 0.3
-    φ_target = 0.7
+# Source: https://www.geometrictools.com/Documentation/RiemannianGeodesics.pdf
+def manifold_to_metric(
+    manifold: Callable[[ArrayM], Array1D]
+) -> Callable[[ArrayM], ArrayMM]:
+    @jax.jit
+    def metric(x: ArrayM) -> ArrayMM:
+        jac: Float[Array, "_ M"] = jax.jacfwd(manifold)(x)
+        return jac.T @ jac
+
+    return metric
+
+
+# Source: https://www.geometrictools.com/Documentation/RiemannianGeodesics.pdf
+def metric_to_christoffel(
+    metric: Callable[[ArrayM], ArrayMM]
+) -> Callable[[ArrayM], ArrayMMM]:
+    def christoffel_first_kind(x: ArrayM) -> ArrayMMM:
+        jac_ijk: ArrayMMM = jax.jacfwd(metric)(x)
+        jac_jki = jnp.transpose(jac_ijk, (1, 2, 0))
+        jac_kij = jnp.transpose(jac_ijk, (2, 0, 1))
+        return 0.5 * (jac_jki + jac_kij - jac_ijk)
+
+    @jax.jit
+    def christoffel_second_kind(x: ArrayM) -> ArrayMMM:
+        Γ: ArrayMMM = christoffel_first_kind(x)
+        g: ArrayMM = metric(x)
+        g_inv = jnp.linalg.inv(g)
+        return jnp.tensordot(g_inv, Γ, axes=((1,), (2,)))
+
+    return christoffel_second_kind
+
+
+# Source: https://www.cefns.nau.edu/~schulz/torus.pdf
+def demo_torus():
+    θ_target = 1
+    φ_target = 2.2
     target = jnp.array([θ_target, φ_target])
-    pieces = 4
+    pieces = 4  # TODO: increasing this doesn't seem to help?
 
-    def Γ(x: Float[Array, "2"]) -> Float[Array, "2 2 2"]:
-        θ, _ = x
+    R = 1
+    r = 0.2
+
+    def manifold(x: Float[Array, "2"]) -> Float[Array, "3"]:
+        assert x.shape == (2,)
+        θ, φ = x
+        tmp = R + r * jnp.cos(φ)
+        x = tmp * jnp.cos(θ)
+        y = tmp * jnp.sin(θ)
+        z = r * jnp.sin(φ)
+        return jnp.stack([x, y, z])
+
+    # Or `metric = manifold_to_metric(manifold)`, but this is more efficient.
+    def metric(x: Float[Array, "2"]) -> Float[Array, "2 2"]:
+        assert x.shape == (2,)
+        _, φ = x
+        g_θθ = (R + r * jnp.cos(φ)) ** 2
+        g_θφ = g_φθ = 0
+        g_φφ = r**2
+        return jnp.array([[g_θθ, g_θφ], [g_φθ, g_φφ]])
+
+    # Or `christoffel = metric_to_christoffel(metric)`, but this is more efficient.
+    def christoffel(x: Float[Array, "2"]) -> Float[Array, "2 2 2"]:
+        assert x.shape == (2,)
+        θ, φ = x
+        tmp = R + r * jnp.cos(φ)
         Γ⁀θ_θθ = 0
-        Γ⁀θ_θφ = 0
-        Γ⁀θ_φθ = 0
-        Γ⁀θ_φφ = -jnp.cos(θ) * jnp.sin(θ)
-        Γ⁀φ_θθ = 0
-        Γ⁀φ_θφ = 1 / jnp.tan(θ)
-        Γ⁀φ_φθ = 1 / jnp.tan(θ)
+        Γ⁀θ_θφ = Γ⁀θ_φθ = -r * jnp.sin(φ) / tmp
+        Γ⁀θ_φφ = 0
+        Γ⁀φ_θθ = tmp * jnp.sin(φ) / r
+        Γ⁀φ_θφ = 0
+        Γ⁀φ_φθ = 0
         Γ⁀φ_φφ = 0
         return jnp.array(
             [[[Γ⁀θ_θθ, Γ⁀θ_θφ], [Γ⁀θ_φθ, Γ⁀θ_φφ]], [[Γ⁀φ_θθ, Γ⁀φ_θφ], [Γ⁀φ_φθ, Γ⁀φ_φφ]]]
         )
 
     def canonicalise(x: Float[Array, "2"]) -> Float[Array, "2"]:
-        # θ is the angle from the vertical
-        # φ is the angle from the horizontal
-        θ, φ = x
-        θ = θ % (2 * jnp.pi)
-        pred = θ < jnp.pi
-        θ = jnp.where(pred, θ, 2 * jnp.pi - θ)
-        φ = jnp.where(pred, φ, jnp.pi + φ)
-        φ = φ % (2 * jnp.pi)
-        return jnp.stack([θ, φ])
+        assert x.shape == (2,)
+        return x % (2 * jnp.pi)
 
-    v0 = solve_bvp(target, pieces, Γ, canonicalise)
-    sol = solve_ivp_dense(0, 1, jnp.zeros_like(target), v0, Γ)
-    θs, φs = jax.vmap(sol, out_axes=1)(jnp.linspace(0, 1, 100))
+    v0 = solve_bvp(
+        target, pieces, christoffel, canonicalise
+    )
+    sol = solve_ivp_dense(0, 1, jnp.zeros_like(target), v0, christoffel)
+    xs = jax.vmap(sol)(jnp.linspace(0, 1, 100))
+    x_path, y_path, z_path = jax.vmap(manifold, out_axes=1)(xs)
+    x0, y0, z0 = manifold(jnp.zeros_like(target))
+    x_target, y_target, z_target = manifold(target)
 
-    # θs = jnp.linspace(0, θ_target, pieces + 1, dtype=target.dtype)
-    # φs = jnp.linspace(0, φ_target, pieces + 1, dtype=target.dtype)
-
-    def to_3d(θ, φ):
-        x = jnp.sin(θ) * jnp.cos(φ)
-        y = jnp.sin(θ) * jnp.sin(φ)
-        z = jnp.cos(θ)
-        return x, y, z
-
-    θ_surface, φ_surface = np.mgrid[0.0 : np.pi : 100j, 0.0 : 2 * np.pi : 100j]
-    x_surface, y_surface, z_surface = to_3d(θ_surface, φ_surface)
-    x_path, y_path, z_path = to_3d(θs, φs)
+    surface = np.mgrid[0.0 : 2 * np.pi : 100j, 0.0 : 2 * np.pi : 100j]
+    x_surface, y_surface, z_surface = jax.vmap(jax.vmap(manifold, in_axes=1, out_axes=1), in_axes=1, out_axes=1)(surface)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-    ax.plot_surface(x_surface, y_surface, z_surface, rstride=1, cstride=1, color="c", alpha=0.7, linewidth=0)
-    ax.plot(1.01 * x_path, 1.01 * y_path, 1.01 * z_path, color="r", linewidth=3)
-    ax.set_xlim([-1, 1])
-    ax.set_ylim([-1, 1])
-    ax.set_zlim([-1, 1])
+    ax.plot_surface(
+        x_surface, y_surface, z_surface, rstride=1, cstride=1, color="c", linewidth=0
+    )
+    ax.plot(
+        1.02 * x_path, 1.02 * y_path, 1.02 * z_path, color="r", linewidth=3, zorder=10
+    )
+    ax.plot(1.02 * x0, 1.02 * y0, 1.02 * z0, "o", zorder=20)
+    ax.plot(1.02 * x_target, 1.02 * y_target, 1.02 * z_target, "o", zorder=20)
+    ax.set_xlim([-1.1, 1.1])
+    ax.set_ylim([-1.1, 1.1])
+    ax.set_zlim([-1.1, 1.1])
     ax.set_aspect("equal")
     plt.show()
 
 
+# def demo_sphere():⁀φ_φφ
+#     # θ_target = 2⁀φ_φφ.1⁀φ_φφ
+#     # φ_target = 0⁀φ_φφ.8⁀φ_φφ
+#     θ_target = 0.3⁀φ_φφ
+#     φ_target = 0.7⁀φ_φφ
+#     target = jnp.a⁀φ_φφrr⁀φ_φφay([θ_target, φ_target])
+#     pieces = 4
+
+#     def christoffe⁀φ_φφl(⁀φ_φφx: Float[Array, "2"]) -> Float[Array, "2 2 2"]:
+#         θ, _ = x
+#         tanθ = jnp⁀φ_φφ.t⁀φ_φφan(θ)
+#         pred = tan⁀φ_φφθ ⁀φ_φφ< 1e-5
+#         safe_tanθ ⁀φ_φφ= ⁀φ_φφjnp.where(pred, 1, tanθ)
+#         cotθ = jnp⁀φ_φφ.w⁀φ_φφhere(pred, jnp.finfo(x.dtype).max, 1 / safe_tanθ)
+#         Γ⁀θ_θθ = 0⁀φ_φφ
+#         Γ⁀θ_θφ = 0
+#         Γ⁀θ_φθ = 0
+#         Γ⁀θ_φφ = -jn⁀φ_φφp.cos(θ) * jnp.sin(θ)
+#         Γ⁀φ_θθ = 0
+#         Γ⁀φ_θφ = cotθ
+#         Γ⁀φ_φθ = cotθ
+#         Γ⁀φ_φφ = 0
+#         return jnp.array(
+#             [[[Γ⁀θ_θθ, Γ⁀θ_θφ], [Γ⁀θ_φθ, Γ⁀θ⁀φφ]], [[Γ⁀φ_θθ, Γ⁀φ_θφ], [Γ⁀φ_φθ, Γ⁀φ_φφ]]]
+#         )
+
+#     def canonicalise(x: Float[Array, "2"]) -> Float[Array, "2"]:
+#         # θ is the angle from the vertical
+#         # φ is the angle from the horizontal
+#         θ, φ = x
+#         θ = θ % (2 * jnp.pi)
+#         pred = θ < jnp.pi
+#         θ = jnp.where(pred, θ, 2 * jnp.pi - θ)
+#         φ = jnp.where(pred, φ, jnp.pi + φ)
+#         φ = φ % (2 * jnp.pi)
+#         return jnp.stack([θ, φ])
+
+#     v0 = solve_bvp(target, pieces, christoffel, canonicalise)
+#     sol = solve_ivp_dense(0, 1, jnp.zeros_like(target), v0, christoffel)
+#     θs, φs = jax.vmap(sol, out_axes=1)(jnp.linspace(0, 1, 100))
+
+#     # θs = jnp.linspace(0, θ_target, pieces + 1, dtype=target.dtype)
+#     # φs = jnp.linspace(0, φ_target, pieces + 1, dtype=target.dtype)
+
+#     def to_3d(θ, φ):
+#         x = jnp.sin(θ) * jnp.cos(φ)
+#         y = jnp.sin(θ) * jnp.sin(φ)
+#         z = jnp.cos(θ)
+#         return x, y, z
+
+#     θ_surface, φ_surface = np.mgrid[0.0 : np.pi : 100j, 0.0 : 2 * np.pi : 100j]
+#     x_surface, y_surface, z_surface = to_3d(θ_surface, φ_surface)
+#     x_path, y_path, z_path = to_3d(θs, φs)
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection="3d")
+#     ax.plot_surface(x_surface, y_surface, z_surface, rstride=1, cstride=1, color="c", alpha=0.7, linewidth=0)
+#     ax.plot(1.01 * x_path, 1.01 * y_path, 1.01 * z_path, color="r", linewidth=3)
+#     ax.set_xlim([-1, 1])
+#     ax.set_ylim([-1, 1])
+#     ax.set_zlim([-1, 1])
+#     ax.set_aspect("equal")
+#     plt.show()
+
+
 if __name__ == "__main__":
-    demo_sphere()
+    demo_torus()
